73e21dc0997129ec03682987f5fefbfca117efdf
xiang1224 <a10083237@gmail.com>
2022-10-02 21:42:26 +0800

    1002-2

--- js/app.js
+++ js/app.js
@@ -2,13 +2,13 @@
 const SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
 const recognition = new SpeechRecognition();
 const mic = document.querySelector('#mic');
-const body = document.querySelector('body');
+const colorful = document.querySelector('#colorful');
 mic.onclick = () => {
   recognition.start();
 }
 recognition.onresult = (event) => {
   const color = event.results[0][0].transcript;
   console.log(color);
-  body.style.backgroundColor = color;
+  colorful.style.backgroundColor = color;
 }
 
